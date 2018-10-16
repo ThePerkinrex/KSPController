0545810fd8c808d8ad1c8da82b9f91e8e9bf775e
theperkinrex <theperkinrex@gmail.com>
2018-10-16 20:58:35 +0200

    Added constants.py Added sending data to be possible by the computer and the arduino

--- KSPController.py
+++ KSPController.py
@@ -5,9 +5,15 @@ from serial.tools.list_ports import comports
 from logger import *
 import time
 import sys
+from CONSTANTS import *
 
 l.set_minimum_level(DEBUG)
 TIMEOUT = 10
+
+def invert_trunc(n):
+    if n>int(n):
+        return int(n)+1
+    return int(n)
 
 exit_serial_port_find = False
 attempts = 0
@@ -34,7 +40,13 @@ while not exit_serial_port_find:
 info('Serial port connected')
 ser = serial.Serial('/dev/cu.usbmodem14201', 9600, timeout=1)
 info('Serial connection established')
-conn = krpc.connect('Perc Controller', rpc_port=25565, stream_port=25566)
+conn = None
+while conn is None:
+    try:
+        conn = krpc.connect('Perc Controller', rpc_port=25565, stream_port=25566)
+    except:
+        conn = None
+
 info('Connection established with KSP')
 
 def read_line():
@@ -53,35 +65,113 @@ def read_line():
     return string
 
 def write_line(line):
+    debug('Writing:', line)
     ser.write((line+'\n').encode('ascii'))
     ser.flushOutput()
 
+def write_command(commn, *args):
+    comm = str(commn)+':'
+    for arg in args:
+        comm += str(arg)
+        if args.index(arg) != args.count-1:
+            comm += ';'
+    write_line(comm)
+    
+
 def close_connection():
     
-    write_line('4')
+    #write_line(CONNECTION_END_OUT)
+    write_command(CONNECTION_END_OUT)
     ser.close()
     conn.close()
 
 while True:
-    if read_line() == '4':
+    if read_line() == ARDUINO_STARTUP:
         info('Controller initialized')
-        write_line('3')
+        #write_line('3')
         break
 
-#idx = 0
-while True:
-    #debug('Looping')
+lcd_mode = 0
+lcd_max = 1
+# 0: Surface altitude
+# 1: Speed
+lcd_mode_change = False
+connected=False
+def send_data():
+    vessel = conn.space_center.active_vessel
+    if not connected:
+        #write_line('3:'+str(int(vessel.control.rcs))+';'str(int(vessel.control.sas)))
+        write_command(CONNECTION_START_OUT, int(vessel.control.rcs), int(vessel.control.sas))
+        connected = True
+    resources = vessel.resources
+    lf = '0'
+    ox = '0'
+    sf = '0'
+    mp = '0'
+    ec = '0'
     try:
-        #write_line('1:Hello;'+str(idx))
+        lf = str(invert_trunc(resources.amount('LiquidFuel')/resources.max('LiquidFuel')*10))
+    except ZeroDivisionError:
+        pass
+    try:
+        ox = str(invert_trunc(resources.amount('Oxidizer')/resources.max('Oxidizer')*10))
+    except ZeroDivisionError:
+        pass
+    try:
+        sf = str(invert_trunc(resources.amount('SolidFuel')/resources.max('SolidFuel')*10))
+    except ZeroDivisionError:
+        pass
+    try:
+        mp = str(invert_trunc(resources.amount('MonoPropellant')/resources.max('MonoPropellant')*10))
+    except ZeroDivisionError:
+        pass
+    try:
+        ec = str(invert_trunc(resources.amount('ElectricCharge')/resources.max('ElectricCharge')*10))
+    except ZeroDivisionError:
+        pass
+    #write_line('0:'+lf+';'+ox+';'+sf+';'+mp+';'+ec)
+    write_command(FUEL_LEVELS_OUT, lf, ox, sf, mp, ec)
+    if lcd_mode_change:
+        lcd_mode_change = False
+        if lcd_mode == 0:
+            # TODO: Make it with m/k/M like the one in KSP
+            write_command(LCD_TXT_OUT, 'SURFACE ALTITUDE', vessel.flight().surface_altitude)
+            
+        if lcd_mode == 1:
+            # ? Review needed
+            speedmode = vessel.control.speedmode
+            reference_frame = vessel.surface_reference_frame
+            speedname = 'SURFACE'
+            if speedmode == speedmode.orbit:
+                reference_frame = vessel.orbital_reference_frame
+                speedname = 'ORBIT'
+            write_command(LCD_TXT_OUT, speedname + ' SPEED', vessel.flight(reference_frame).surface_altitude)
+
+
+while True:
+    try:
         if conn.krpc.current_game_scene == conn.krpc.GameScene.flight:
-            vessel = conn.space_center.active_vessel
-            resources = vessel.resources
-            debug('SolidFuel:', resources.amount('SolidFuel'))
+            
+            send_data()
             line = read_line()
             if line != '':
                 debug(line)
-        #idx += 1
-        #time.sleep(0.5)
+                if line == MODE_UP_IN:
+                    lcd_mode_change = True
+                    lcd_mode += 1
+                    if lcd_mode > lcd_max:
+                        lcd_mode = 0
+                elif line == MODE_DOWN_IN:
+                    lcd_mode_change = True
+                    lcd_mode -= 1
+                    if lcd_mode < 0:
+                        lcd_mode = lcd_max
+        else:
+            if connected:
+                write_command(CONNECTION_END_OUT)
+                connected = False
+
+        
     except Exception as e:
         err("Error:", e)
         break
--- kspController/kspController.ino
+++ kspController/kspController.ino
@@ -13,29 +13,98 @@
 #define connectionEnded 4
 
 // PINS
-#define DATA_PIN 2
-#define LATCH_PIN 3
-#define CLOCK_PIN 4
-#define CONNECTED_LED_PIN 5
+// DIGITAL
+// I/O CUSTOM
+#define MODE_CLOCK_PIN 2
+#define MODE_DATA_PIN 3
+#define LVL_DATA_PIN 4
+#define LVL_LATCH_PIN 5
+#define LVL_CLOCK_PIN 6
+// LEDS (OUTPUT)
+#define CONNECTED_LED_PIN 7
+#define RCS_LED_PIN 8
+#define SAS1_LED_PIN 9
+#define SAS2_LED_PIN 10
+#define MANOUVER_LED_PIN 11
+// SWITCHES & BUTTONS (INPUT)
+#define MODE_SW_PIN 12
+#define XY_JOY_SW_PIN 22
+#define Z_JOY_SW_PIN 23
+// SWITCHES & BUTTONS (INPUT_PULLUP)
+#define SAS_BTN_1_PIN 40
+#define SAS_BTN_2_PIN 41
+#define SAS_BTN_3_PIN 42
+#define SAS_BTN_4_PIN 43
+#define SAS_BTN_5_PIN 44
+#define SAS_BTN_6_PIN 45
+#define SAS_BTN_7_PIN 46
+#define SAS_BTN_8_PIN 47
+#define STAGE_PIN 48
+#define LIGHTS_PIN 49
+#define GEAR_PIN 50
+#define BRAKES_PIN 51
+#define SAS_SW_PIN 52
+#define RCS_SW_PIN 53
+// ------------------------------
+// ANALOG
+#define THROTTLE_PIN A0
+#define X_JOY_PIN A1
+#define Y_JOY_PIN A2
+#define Z_JOY_PIN A3
+#define P_JOY_PIN A4
 
-#include "BitbloqLiquidCrystal.h"
+#include <BitbloqLiquidCrystal.h>
 
 LiquidCrystal lcd(0);
+bool RCS = false;
+bool SAS = false;
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  //Serial.println("Serial init");
   lcd.begin(16, 2); //inicializamos el LCD, 16 columnas, 2 filas
   lcd.clear(); //borramos cualquier contenido residual
-  pinMode(DATA_PIN, OUTPUT);
-  pinMode(CLOCK_PIN, OUTPUT);
-  pinMode(LATCH_PIN, OUTPUT);
+  //Serial.println("LCD init");
+  // PIN MODES
+  pinMode(MODE_CLOCK_PIN,INPUT_PULLUP);
+  pinMode(MODE_DATA_PIN,INPUT_PULLUP);
+
+  pinMode(LVL_DATA_PIN, OUTPUT);
+  pinMode(LVL_CLOCK_PIN, OUTPUT);
+  pinMode(LVL_LATCH_PIN, OUTPUT);
+
   pinMode(CONNECTED_LED_PIN, OUTPUT);
+  pinMode(RCS_LED_PIN, OUTPUT);
+  pinMode(SAS1_LED_PIN, OUTPUT);
+  pinMode(SAS2_LED_PIN, OUTPUT);
+  pinMode(MANOUVER_LED_PIN, OUTPUT);
+
+  pinMode(MODE_SW_PIN, INPUT);
+  pinMode(XY_JOY_SW_PIN, INPUT);
+  pinMode(Z_JOY_SW_PIN, INPUT);
+
+  pinMode(SAS_BTN_1_PIN, INPUT_PULLUP);
+  pinMode(SAS_BTN_2_PIN, INPUT_PULLUP);
+  pinMode(SAS_BTN_3_PIN, INPUT_PULLUP);
+  pinMode(SAS_BTN_4_PIN, INPUT_PULLUP);
+  pinMode(SAS_BTN_5_PIN, INPUT_PULLUP);
+  pinMode(SAS_BTN_6_PIN, INPUT_PULLUP);
+  pinMode(SAS_BTN_7_PIN, INPUT_PULLUP);
+  pinMode(SAS_BTN_8_PIN, INPUT_PULLUP);
+  pinMode(STAGE_PIN, INPUT_PULLUP);
+  pinMode(LIGHTS_PIN, INPUT_PULLUP);
+  pinMode(GEAR_PIN, INPUT_PULLUP);
+  pinMode(BRAKES_PIN, INPUT_PULLUP);
+  pinMode(SAS_SW_PIN, INPUT_PULLUP);
+  pinMode(RCS_SW_PIN, INPUT_PULLUP);
+  // PIN SETUPS
   digitalWrite(CONNECTED_LED_PIN, LOW);
-  digitalWrite(CLOCK_PIN, LOW);
-  digitalWrite(LATCH_PIN, LOW);
-  Serial.println(startedUp);
+  digitalWrite(LVL_CLOCK_PIN, LOW);
+  digitalWrite(LVL_LATCH_PIN, LOW);
+  // INTERRUPTS
+  attachInterrupt(digitalPinToInterrupt(MODE_CLOCK_PIN), doEncode, CHANGE);
 }
 String tok = "";
 int commNtmp = -1;
@@ -72,28 +141,17 @@ void loop() {
 }
 
 void handleCommand(int commandN, int argc, String argv[]){
-  /*Serial.print(commandN);
-  Serial.print(";");
-  Serial.print(argc);
-  Serial.print(";[");
-  for(int i=0;i<argc;i++){
-    Serial.print(argv[i]);
-    if (i!=argc-1){
-      Serial.print(",");
-    }
-  }
-  Serial.println("]");*/
-  if(commandN==startingUp){
+  if(commandN==startingUp&&argc==2){
     digitalWrite(CONNECTED_LED_PIN, HIGH);
-  }
-  if(commandN==connectionEnded){
+    RCS = bool(argv[0].toInt());
+    SAS = bool(argv[1].toInt());
+  }else if(commandN==connectionEnded){
     digitalWrite(CONNECTED_LED_PIN, LOW);
     lcd.clear();
     levelShiftOut(0,0,0,0,0);
-  }
-
-
-  if(commandN==lcdtxt&&argc==2){
+    RCS = false;
+    SAS = false;
+  }else if(commandN==lcdtxt&&argc==2){
     lcd.clear();
     lcd.p(argv[0].c_str());
     lcd.setCursor(0,1);
@@ -103,8 +161,15 @@ void handleCommand(int commandN, int argc, String argv[]){
   }
 }
 
+bool digitalValues[16];
+
 void sendData(){
-  
+  if modeState == 1 {
+    Serial.println(modeUp);
+  }else if modeState == 2 {
+    Serial.println(modeDown);
+  }
+
 }
 
 void levelShiftOut(byte lf, byte ox, byte sf, byte mp, byte ec){
@@ -115,18 +180,35 @@ void levelShiftOut(byte lf, byte ox, byte sf, byte mp, byte ec){
   levels[3] = short(round(pow(2, constrain(ox, 0, 10))-1)); // Fourth level
   levels[4] = short(round(pow(2, constrain(lf, 0, 10))-1)); // Fifth level
   for(int i=0;i<5;i++){ // For each level
-    Serial.print("Level "); Serial.println(i);
+    //Serial.print("Level "); Serial.println(i);
     for(int j=9;j>=0;j--){ // For each bit of the 10 bits of each level
-      Serial.print(bitRead(levels[i], j));
-      digitalWrite(DATA_PIN, bitRead(levels[i], j)); // Write the bit
-      digitalWrite(CLOCK_PIN, HIGH); // Pulse the clock
+      //Serial.print(bitRead(levels[i], j));
+      digitalWrite(LVL_DATA_PIN, bitRead(levels[i], j)); // Write the bit
+      digitalWrite(LVL_CLOCK_PIN, HIGH); // Pulse the clock
       delay(1);
-      digitalWrite(CLOCK_PIN, LOW);
+      digitalWrite(LVL_CLOCK_PIN, LOW);
     }
-    Serial.println();
+    //Serial.println();
   }
-  digitalWrite(LATCH_PIN, HIGH); // Latch the data
+  digitalWrite(LVL_LATCH_PIN, HIGH); // Latch the data
   delay(1);
-  digitalWrite(LATCH_PIN, LOW);
+  digitalWrite(LVL_LATCH_PIN, LOW);
 }
 
+byte modeState = 0;
+// 0 -> NOP
+// 1 -> UP
+// 2 -> DOWN
+const int timeThreshold = 5;
+long timeCounter = 0;
+
+void doEncode(){
+   if (millis() > timeCounter + timeThreshold){
+      if (digitalRead(MODE_CLOCK_PIN) == digitalRead(MODE_DATA_PIN)){
+        modeState = 1;
+      }else{
+        modeState = 2;
+      }
+      timeCounter = millis();
+   }
+}
